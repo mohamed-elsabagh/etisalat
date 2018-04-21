dbf47ea545dd29ebf5f6823e93a9ccdf04b2e567
Mohamed El-Sabagh <mohamed.elsabagh88@gmail.com>
2018-04-21 07:23:23 +0200

    fixed project folder name

---
+++ mobileapp/server-node/utils/constants.js
@@ -0,0 +1,11 @@
+const port = 6000;
+const databaseHost = 'localhost';
+const databasePort = '27017';
+const appDatabaseName = 'etisalat';
+
+module.exports = {
+    port: port,
+    databaseHost : databaseHost,
+    databasePort : databasePort,
+    appDatabaseName: appDatabaseName
+};
